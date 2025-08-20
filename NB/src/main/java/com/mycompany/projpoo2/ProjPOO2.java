a37923c31f4cb031cf36ff4fa88f2da998904409
Rafael M <rafamunhozcastro@gmail.com>
2025-08-20 09:13:02 -0300

    init

---
+++ NB/src/main/java/com/mycompany/projpoo2/ProjPOO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projpoo2;
+
+/**
+ *
+ * @author rafin
+ */
+public class ProjPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
