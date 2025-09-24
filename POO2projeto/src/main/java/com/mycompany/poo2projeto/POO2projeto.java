bbcd8b0aa596c124ae26ae20b278e146ececcbd1
Rafael M <rafamunhozcastro@gmail.com>
2025-09-24 17:07:31 -0300

    toma

---
+++ POO2projeto/src/main/java/com/mycompany/poo2projeto/POO2projeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo2projeto;
+
+/**
+ *
+ * @author rafin
+ */
+public class POO2projeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
