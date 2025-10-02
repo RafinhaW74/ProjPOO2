1de3908aaefbb39b42ee1bcf83ec8d377ad47393
Vinicius Souza Dias <62678753+vinicius-10@users.noreply.github.com>
2025-10-02 16:09:26 -0300

    aplicacao do modelo SLI para importacao

---
+++ Controller/Altera_importacao.java
@@ -0,0 +1,5 @@
+public class Altera_importacao {
+    public static void altera_importacao(Importacao importacao, Importacao importacao_update){
+        //atualiza a informação e envia para o banco de dados
+    }
+}
