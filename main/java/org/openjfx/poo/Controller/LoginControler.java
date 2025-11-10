696c4f84f3e56412b70103af6ea0705893f44438
Vinicius Souza Dias <62678753+vinicius-10@users.noreply.github.com>
2025-11-10 15:13:22 -0300

    tela de solicitar importacao

--- main/java/org/openjfx/poo/Controller/LoginControler.java
+++ main/java/org/openjfx/poo/Controller/LoginControler.java
@@ -13,17 +13,23 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 /**
  * FXML Controller class
  *
  * @author valuc
  */
 public class LoginControler implements Initializable {
-
+    @FXML
+    private Button btnRegister;
     @FXML
     private Label rtTitle;
     @FXML
@@ -38,8 +44,6 @@ public class LoginControler implements Initializable {
     private TextField tfIdentificator;
     @FXML
     private Button btnSubmit;
-    @FXML
-    private Button btnRegister;
         /**
      * Initializes the controller class.
      */
@@ -53,9 +57,6 @@ public class LoginControler implements Initializable {
 
     }
 
-    private void viewPending(ActionEvent event) {
-
-    }
 
     private void user(ActionEvent event) {
 
@@ -68,5 +69,6 @@ public class LoginControler implements Initializable {
     @FXML
     private void register(ActionEvent event) {
     }
+
     
 }
